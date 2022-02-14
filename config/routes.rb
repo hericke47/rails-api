f3e323610e1de2238010112f91f8fe4bba715bc5
Herick Exterkoetter <herickherick47@gmail.com>
2022-02-14 17:11:15 -0300

    feat: create seed and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :posts
+      resources :users
+    end
+  end
 end
